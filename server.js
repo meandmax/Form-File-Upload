a8f7094bbe8ee6f1507420de99f18e290b0870ce
Clemens Akens <clebert@me.com>
2014-08-13 19:11:57 +0200

    Add basic file upload backend code

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 'use strict';
 
+let fs = require('fs');
+let parse = require('co-busboy');
+let route = require('koa-route');
 let serve = require('koa-static');
 let koa   = require('koa');
 let app   = koa();
 
 const PORT = 3000;
 
+// process file uploads
+app.use(route.post('/process', function *() {
+    if (this.request.is('multipart/*')) {
+        let parts = parse(this, {
+            autoFields: true
+        });
+
+        let part;
+
+        while (part = yield parts) {
+            part.pipe(fs.createWriteStream('uploads/' + (part.filename || 'nofile')));
+        }
+    }
+
+    this.redirect('/');
+}));
+
+// static files
 app.use(serve('demo'));
 app.use(serve('vendor'));
 

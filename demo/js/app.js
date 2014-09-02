193bfa899aebbee9469ba14533f31e038a80f415
Maximilian Heinz <maximilian.heinz@sinnerschrader.com>
2014-09-02 22:57:46 +0200

    Removed jQuery completly

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,8 +1,4 @@
 var initializeFileUpload = function() {
-	var $fileUpload = $('.js_fileupload');
-	var $fileSelect = $fileUpload.find('.js_selectfile');
-	var $dropBox    = $fileUpload.find('.js_dropbox');
-
 	var fileUpload = document.querySelector('.js_fileupload');
 	var fileSelect = document.querySelector('.js_selectfile');
 	var dropBox    = document.querySelector('.js_dropbox');

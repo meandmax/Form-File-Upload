64034788b0b790880e7df548fa3f76bcf2ad5e02
Maximilian Heinz <maximilian.heinz@sinnerschrader.com>
2014-09-03 15:58:32 +0200

    Added some mocks & tests

--- test/easyformfieluploadTest.js
+++ test/easyformfieluploadTest.js
@@ -4,18 +4,30 @@ var sinon              = require('sinon');
 var expect             = require('expect.js');
 var mocha              = require('mocha');
 
-document               = sinon.spy();
+global.document = {
+	querySelector: sinon.spy(),
+	createElement: sinon.spy(),
+	querySelectorAll: sinon.spy()
+};
+
+global.window = {
+	File: true,
+	FileReader: true,
+	FileList: true
+}
 
 var optionsMock        = sinon.spy();
 var fileUploadElMock   = sinon.spy();
-var dropBoxElMock      = sinon.spy();
+var dropBoxElMock      = {
+	addEventListener: sinon.spy()
+};
 
 describe('easyformfileupload', function() {
-	//var easyformfileupload = new EasyFormFileUpload(fileUploadElMock, dropBoxElMock, optionsMock);
+	var easyformfileupload = new EasyFormFileUpload(fileUploadElMock, dropBoxElMock, optionsMock);
 
 	describe('the public api', function(){
-		it('should expose a function', function() {
-		//	expect(easyformfileupload.dndHandler).to.be.a('function');
+		it('should expose the function dndHandler', function() {
+			expect(easyformfileupload.dndHandler).to.be.a('function');
 		})
 	})
 })

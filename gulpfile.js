33141996b168ca5b0ef836bbba3ffdad9e11769e
Maximilian Heinz <maximilian.heinz@sinnerschrader.com>
2014-10-24 09:54:02 +0200

    Added jQuery dist to demo & cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,53 +4,30 @@ var less          = require('gulp-less');
 var csso          = require('gulp-csso');
 var watch         = require('gulp-watch');
 var uglify        = require('gulp-uglify');
-var livereload    = require('gulp-livereload');
-var sourcemaps    = require('gulp-sourcemaps');
 var jshint        = require('gulp-jshint');
-var jshintStylish = require('jshint-stylish');
 var rename        = require('gulp-rename');
 var mocha         = require('gulp-mocha');
 var concat        = require('gulp-concat');
+var jshintStylish = require('jshint-stylish');
 
-gulp.task('less', function() {
-	var stream = gulp.src('./src/less/app.less')
-		.pipe(less())
-		.pipe(sourcemaps.init())
-			.pipe(csso())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./demo'));
-});
-
-gulp.task('scripts', function() {
-	// Single entry point to browserify
-	var stream = gulp.src('./src/js/formfileupload.js')
-		.pipe(browserify({standalone: 'FormFileUpload'}))
-		.pipe(gulp.dest('./dist'))
-		.pipe(gulp.dest('./demo/js'));
-});
-
-gulp.task('minify', function() {
-	var stream = gulp.src('./demo/js/formfileupload.js')
-		.pipe(rename(function (path) {
-			path.basename += ".min";
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('./dist'))
-		.pipe(gulp.dest('./demo/js'));
-});
-
+/**
+ * jshint task for all javascript files
+ */
 gulp.task('lint', function() {
 	return gulp.src('./src/js/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter(jshintStylish));
 });
 
+/**
+ * executes the javascript tests
+ */
 gulp.task('test', function() {
 	return gulp.src('./test/*.js')
-		.pipe(mocha({reporter: 'nyan'}));
+		.pipe(mocha());
 });
 
-gulp.task('jquery', function() {
+gulp.task('jQuery', function() {
 	return gulp.src(['./src/js/formfileupload.js', './src/js/jquery.plugin.js'])
 		.pipe(concat('jquery.formfileupload.js'))
 		.pipe(browserify())
@@ -64,16 +41,36 @@ gulp.task('jquery', function() {
 		.pipe(gulp.dest('./demo/js'));
 });
 
-gulp.task('dist', ['scripts'], function() {
-	gulp.src('./src/**/*.js')
-		.pipe(gulp.dest('./dist/'));
+/**
+ * Build task for the final css files
+ */
+gulp.task('less', function() {
+	var stream = gulp.src('./src/less/app.less')
+		.pipe(less())
+		.pipe(csso())
+		.pipe(gulp.dest('./demo'));
 });
 
-gulp.task('watch', function() {
-	// calls 'build-js' whenever anything changes
+/**
+ * Build task for the final javascript files
+ */
+gulp.task('scripts', ['jQuery', 'lint', 'test'], function() {
+	// Single entry point to browserify
+	var stream = gulp.src('./src/js/formfileupload.js')
+		.pipe(browserify({standalone: 'FormFileUpload'}))
+		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest('./demo/js'))
+		.pipe(rename(function (path) {
+			path.basename += ".min";
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest('./demo/js'));
+});
+
+gulp.task('dev', function() {
 	gulp.watch('./src/js/**/*.js', ['scripts', 'test', 'lint']);
 	gulp.watch('./src/less/**/*.less', ['less']);
-	gulp.watch('./demo/js/formfileupload.js', ['minify']);
 });
 
-gulp.task('default', ['scripts', 'less', 'lint', 'test', 'minify']);
+gulp.task('build', ['scripts', 'less']);
